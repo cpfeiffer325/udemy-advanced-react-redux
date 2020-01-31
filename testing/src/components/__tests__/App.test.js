4284c38b469f067a44dd874c2a4dcedc97d4a920
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-30 18:50:48 -0700

    create first test for App component

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,4 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
+import CommentBox from '../CommentBox'
 
+it('Should show a comment box', () => {
+  const wrapped = shallow(<App />)
+
+  expect(wrapped.find(CommentBox).length).toEqual(1)
+})

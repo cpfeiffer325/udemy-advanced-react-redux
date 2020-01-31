55155384dfc9a9976e04dd9edd389eeeda9980c4
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-30 22:59:13 -0700

    add test for comment list

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -2,9 +2,16 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
 import CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
 
 it('Should show a comment box', () => {
   const wrapped = shallow(<App />)
 
   expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('Should show a comment list', () => {
+  const wrapped = shallow(<App />)
+
+  expect(wrapped.find(CommentList).length).toEqual(1)
 })

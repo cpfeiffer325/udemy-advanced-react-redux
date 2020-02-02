27ef72026e3dc39fce956604b8b7ba741b07d59d
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-02-01 21:48:24 -0700

    refactor test with a beforeEach for duplicated code

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -4,14 +4,16 @@ import App from 'components/App'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-it('Should show a comment box', () => {
-  const wrapped = shallow(<App />)
+let wrapped
 
+beforeEach(() => {
+  wrapped = shallow(<App />)
+})
+
+it('Should show a comment box', () => {
   expect(wrapped.find(CommentBox).length).toEqual(1)
 })
 
 it('Should show a comment list', () => {
-  const wrapped = shallow(<App />)
-
   expect(wrapped.find(CommentList).length).toEqual(1)
 })

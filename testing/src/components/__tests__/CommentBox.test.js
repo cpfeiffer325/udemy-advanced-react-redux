0f2be55d337dcdf677e7e6d663d82968369925f4
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-02-02 22:49:45 -0700

    create Comment box assertion to check that user can input  into text area

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -19,3 +19,11 @@ it('should have a text area', () => {
 it('should have a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
+
+it('should have a text area that a user can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  })
+  wrapped.update()
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+})

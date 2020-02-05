e688cb8730628d566c5316ca97fc5736ab0f186f
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-02-04 23:52:58 -0700

    map state to props and create render button function

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
 export default () => {
+  renderButton() {
+    if (this.props.auth) {
+      return <button>Sign Out</button>
+    } else {
+      return <button>Sign In</button>
+    }
+  }
+
   return (
     <div>
+      {this.renderHeader()}
       <Route path="/post" component={CommentBox} />
       <Route path="/" exact component={CommentList} />
     </div>
   )
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(App)
